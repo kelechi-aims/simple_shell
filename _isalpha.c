2db337f2294b0ee806c73b087ecf03c37f3aa4b2
yaweffah03 <mr.effah@icloud.com>
2023-05-26 16:48:40 -0700

    Update _isalpha.c

--- _isalpha.c
+++ _isalpha.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
 /**
- * is_delim - checks if character is a delimeter
- * @c: the char to checked
+ * is_delim - It checks if the character is a delimeter
+ * @c: the char to be checked
  * @delim: the delimeter string
- * Return: 1 if true, 0 if false
+ * Return: If true,1 , if false, 0
  */
 int is_delim(char c, char *delim)
 {
@@ -15,9 +15,9 @@ int is_delim(char c, char *delim)
 }
 
 /**
- * _isalpha - checks for alphabetic character
+ * _isalpha - checks for alphabet characters
  * @c: The char input
- * Return: 1 if c is alphabetic, 0 otherwise
+ * Return: if c is alphabetic, 1, 0 if otherwise
  */
 
 int _isalpha(int c)
@@ -29,7 +29,7 @@ int _isalpha(int c)
 }
 
 /**
- * _atoi - converts a string to an integer
+ * _atoi - this converts a string to an integer
  * @s: parameter of the function
  *
  * Return: value 0
